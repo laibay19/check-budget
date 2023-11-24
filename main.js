4d775d63d58ae0b09d9d5ce8a3f8d61b1e1f452b
laibay <150113457+laibay19@users.noreply.github.com>
2023-11-24 13:33:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function num() {
+    let expenses = +document.getElementById("expenses").value
+    let bugde = +document.getElementById("budge").value
+
+
+    if( bugde >= expenses){
+         "you are elegible"
+    }else{
+        "you are not alegible"
+    }
+    
+    let output = document.getElementById("result")
+    output.innerHTML=(result)
+}
